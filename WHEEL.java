deee8a847be2442341785f44cd91058319984462
georgeerel <georgeerel1@gmail.com>
2025-06-26 19:00:58 +0300

    Bulid WHEEL class

---
+++ WHEEL.java
@@ -0,0 +1,8 @@
+public class WHEEL {
+    private double Radius; //רדיוס
+    private double RotationSpeed=0; // מהירות סיבובית
+    private String Name; //מיקום על הרובוט
+    private double Circumference; // היקף
+
+    
+}
