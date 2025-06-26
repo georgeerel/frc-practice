79539d23816557d162f9c76d6d839675674cee61
georgeerel <georgeerel1@gmail.com>
2025-06-26 18:52:24 +0300

    add Constructor

--- MOTOR.java
+++ MOTOR.java
@@ -1,7 +1,10 @@
-public class MOTOR 
-{
+public class MOTOR{
     private double speed=0;
     private String Name;
     private boolean isReversed;
-
+    
+    public MOTOR(String name){
+        this.Name = name;
+        isReversed = false;
+    }
 }
